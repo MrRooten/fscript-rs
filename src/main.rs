57908c1c84ccfeb7b8794ed92bbae88e0b06c7ec
123 <25635931+Mten@usb.com>
2024-04-10 01:14:15 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
+use fscript_rs::{
+    backend::{
+        compiler::bytecode::Bytecode,
+        vm::{runtime::FSRVM, thread::FSRThreadRuntime},
+    },
+    frontend::ast::token::{
+        base::{FSRMeta, FSRToken},
+        module::FSRModuleFrontEnd,
+    },
+};
 
 fn main() {
-    
+    let expr = "
+        a = 1
+        b = a + a
+        println(b)
+        ";
+    let meta = FSRMeta::new();
+    let token = FSRModuleFrontEnd::parse(expr.as_bytes(), meta).unwrap();
+    let v = Bytecode::load_ast(FSRToken::Module(token));
+    let mut runtime = FSRThreadRuntime::new();
+    let mut vm = FSRVM::new();
+    runtime.start(v, &mut vm);
 }
