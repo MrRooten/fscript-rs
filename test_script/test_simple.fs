1552fd27b6804e53eff31c19538d6fb04edc82a5
hello_world <junzun@sdf.com>
2025-06-20 18:48:41 +0800

    add test

--- test_script/test_simple.fs
+++ test_script/test_simple.fs
@@ -1,7 +1,10 @@
-try {
-    for i in 0..3000000 {
-        [1, Exception()]
-    }
-} catch {
-    println("An error occurred during the loop execution.")
-}
+a = 2
+a = a.then(|x| {
+    println("Hello World")
+    return 2
+}).map_err(|x| {
+    println("Error happened")
+    return 1
+})
+
+println(a)
