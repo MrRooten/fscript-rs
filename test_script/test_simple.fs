9c0657b672d4cbaf630ca52f9a92c9adff67dbad
asdf <asdf@exa.com>
2026-01-09 18:54:49 +0800

    update readme

--- test_script/test_simple.fs
+++ test_script/test_simple.fs
@@ -1,12 +1,19 @@
 struct Test {
     field: u64
+    field2: u64
+}
+
+@static
+fn simple(n: u64) -> Ptr[Test] {
+    ptr: Ptr[Test] = Test.alloc
+    ptr.field2 = n
+    return ptr
 }
 
 @entry
 fn test() -> u64 {
-    a: Ptr[Test] = Test.alloc
-    a.field = 42
-    return a.field
+    out_ptr: Ptr[Test] = simple(2)
+    return out_ptr.field2
 }
 
 a = test()
